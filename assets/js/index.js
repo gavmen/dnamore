05fa6aad152bc2e40d319921d5e5a3151606364b
Gabriel Mendonca <gabriel.mendonca@okn.com.br>
2023-07-23 20:37:58 -0300

    responsivo e ajustes

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,5 @@
+// LOAD ANIMATION
+
 const handleIntersection = (entries, observer) => {
     entries.forEach(entry => {
         if (entry.isIntersecting) {
@@ -19,4 +21,18 @@ const hiddenElements = document.querySelectorAll(".hidden");
 
 hiddenElements.forEach(element => {
     observer.observe(element);
+});
+
+// BOTAO MENU MOBILE
+
+const navIcon = document.getElementById('nav-icon4');
+const menuMobile = document.getElementsByClassName('mobile-menu');
+
+console.log(menuMobile);
+
+navIcon.addEventListener('click', function() {
+  this.classList.toggle('open');
+  if (menuMobile.length > 0) {
+    menuMobile[0].classList.toggle('mobile-menu--open');
+  }
 });
